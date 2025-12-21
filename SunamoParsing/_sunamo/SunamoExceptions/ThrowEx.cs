797e576c1e84d8982bbdee7caecfd200a76228f3
Radek Jancik <radek.jancik@usys.cz>
2025-12-21 21:25:46 +0100

    25.12.21.1 feat: Added net10.0 to TargetFrameworks

--- SunamoParsing/_sunamo/SunamoExceptions/ThrowEx.cs
+++ SunamoParsing/_sunamo/SunamoExceptions/ThrowEx.cs
@@ -1,6 +1,7 @@
+namespace SunamoParsing._sunamo.SunamoExceptions;
+
 // EN: Variable names have been checked and replaced with self-descriptive names
 // CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
-namespace SunamoParsing._sunamo.SunamoExceptions;
 internal partial class ThrowEx
 {
 
